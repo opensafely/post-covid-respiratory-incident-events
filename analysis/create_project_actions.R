148dcccd807dedee69793dcd53432a19ecd2bbbb
RochelleKnight <rochelle.knight@bristol.ac.uk>
2023-05-04 14:45:07 +0100

    uncomment table 1

--- analysis/create_project_actions.R
+++ analysis/create_project_actions.R
@@ -306,17 +306,17 @@ actions_list <- splice(
     )
   ),
   
-  # comment("Stage 2 - Missing - Table 1"),
-  # action(
-  #   name = "stage2_missing_table1_all",
-  #   run = "r:latest analysis/descriptives/Stage2_missing_table1.R all",
-  #   needs = list("stage1_data_cleaning_all"),
-  #   moderately_sensitive = list(
-  #     Missing_RangeChecks = glue("output/not-for-review/Check_missing_range_*.csv"),
-  #     DateChecks = glue("output/not-for-review/Check_dates_range_*.csv"),
-  #     Descriptive_Table = glue("output/review/descriptives/Table1_*.csv")
-  #   )
-  # ),
+  comment("Stage 2 - Missing - Table 1"),
+  action(
+    name = "stage2_missing_table1_all",
+    run = "r:latest analysis/descriptives/Stage2_missing_table1.R all",
+    needs = list("stage1_data_cleaning_all"),
+    moderately_sensitive = list(
+      Missing_RangeChecks = glue("output/not-for-review/Check_missing_range_*.csv"),
+      DateChecks = glue("output/not-for-review/Check_dates_range_*.csv"),
+      Descriptive_Table = glue("output/review/descriptives/Table1_*.csv")
+    )
+  ),
 
   comment("Stage 5 - Run models"),
   splice(
